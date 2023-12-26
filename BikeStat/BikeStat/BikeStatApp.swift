70f966596f0fc4041bff02c6ea6c4804d768ffe8
kupriyanovNik <125807834+kupriyanovNik@users.noreply.github.com>
2023-12-26 20:26:52 +0300

    Add files via upload

---
+++ BikeStat/BikeStat/BikeStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeStatApp.swift
+//  BikeStat
+//
+//  Created by Никита Куприянов on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BikeStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BikeStat/BikeStat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BikeStat
+//
+//  Created by Никита Куприянов on 26.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
