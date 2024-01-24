d7564bcde95e66d5bf2752b1b7630445b9a8665e
kupriyanovNik <cucuprianov@icloud.com>
2024-01-24 20:40:05 +0300

    sample UI of StatisticsView

--- BikeStat/BikeStat/PresentationLayer/Modules/Statistics/StatisticsView.swift
+++ BikeStat/BikeStat/PresentationLayer/Modules/Statistics/StatisticsView.swift
@@ -8,12 +8,32 @@ struct StatisticsView: View {
 
     // MARK: - Property Wrappers
 
+    @Environment(\.dismiss) var dismiss
+
     @ObservedObject var coreDataManager: CoreDataManager
 
     // MARK: - Body
 
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+        }
+        .safeAreaInset(edge: .top, content: headerView)
+    }
+
+    // MARK: - View Builders
+
+    @ViewBuilder func headerView() -> some View {
+        Text("Статистика")
+            .hCenter()
+            .overlay(alignment: .leading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: Images.back)
+                        .padding()
+                }
+            }
     }
 }
 
