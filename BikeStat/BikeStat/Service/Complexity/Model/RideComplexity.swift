8a5ed517bd920de9e7bbd3af0af51349c17fc528
kupriyanovNik <cucuprianov@icloud.com>
2024-01-24 20:04:57 +0300

    added new properties to complexity model

--- BikeStat/BikeStat/Service/Complexity/Model/RideComplexity.swift
+++ BikeStat/BikeStat/Service/Complexity/Model/RideComplexity.swift
@@ -2,7 +2,7 @@
 //  RideComplexity.swift
 //
 
-import Foundation
+import SwiftUI
 
 enum RideComplexity {
     case easy, medium, hard, `unowned`
@@ -17,6 +17,32 @@ enum RideComplexity {
             "Сложный"
         case .unowned:
             "хз"
+        }
+    }
+
+    var estimatedComplexityColor: Color {
+        switch self {
+        case .easy:
+            Pallete.EstimatedComplexity.easy
+        case .medium:
+            Pallete.EstimatedComplexity.medium
+        case .hard:
+            Pallete.EstimatedComplexity.hard
+        case .unowned:
+            .clear
+        }
+    }
+
+    var realComplexityColor: Color {
+        switch self {
+        case .easy:
+            Pallete.Complexity.easy
+        case .medium:
+            Pallete.Complexity.medium
+        case .hard:
+            Pallete.Complexity.hard
+        case .unowned:
+            .clear
         }
     }
 }
