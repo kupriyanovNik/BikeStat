4265d34ae54fb19bdbefc58154b1f816171c9e48
kupriyanovNik <cucuprianov@icloud.com>
2024-02-02 09:54:51 +0300

    fixed user weight

--- BikeStat/BikeStat/PresentationLayer/Modules/Settings/SettingsViewModel.swift
+++ BikeStat/BikeStat/PresentationLayer/Modules/Settings/SettingsViewModel.swift
@@ -9,6 +9,6 @@ class SettingsViewModel: ObservableObject {
     // MARK: - Property Wrappers
 
     @AppStorage("SETTINGS_DistanceUnits") var isMetricUnits: Bool = true
-    @AppStorage("SETTINGS_UserWeight") var userWeight: Int = 60
+    @AppStorage("SETTINGS_UserWeight") var userWeight: Int = 75
 
 }
