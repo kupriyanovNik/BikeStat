fa8cbe1a1c02d9742d552391729a67be571c5ef9
kupriyanovNik <cucuprianov@icloud.com>
2024-02-03 17:39:11 +0300

    added recommendations texts

--- BikeStat/BikeStat/PresentationLayer/Modules/Statistics/StatisticsChartDataModel.swift
+++ BikeStat/BikeStat/PresentationLayer/Modules/Statistics/StatisticsChartDataModel.swift
@@ -19,10 +19,25 @@ struct RecommendationsChartDataModel: Identifiable, Equatable {
     var complexity: String
 }
 
-struct StatisticsModel {
-    static let easyRecommendations = [String]()
+struct RecommendationTextModel {
+    static let easyRecommendations = [
+        "Начинайте с коротких поездок, чтобы ваш организм мог постепенно адаптироваться к физической активности.",
+        "Пейте много воды, чтобы избежать обезвоживания.",
+        "Следите за Вашим самочувствием. Если что-то болит, отдохните, иначе могут быть плохие последствия.",
+        "Делайте качественную разминку перед тренировками."
+    ]
 
-    static let mediumRecommendations = [String]()
+    static let mediumRecommendations = [
+        "Не забывайте следить за состоянием Вашего велосипеда и собственным здоровьем!",
+        "Находите новых друзей и катайтесь вместе, это полезно и весело!",
+        "Обязательно поучавствуйте в открытом велосипедном марафоне!",
+        "Со временем увеличивайте скорость. Попробуйте [HIIT](https://www.championat.com/lifestyle/article-4150673-chto-takoe-hiit-trenirovki-pljusy-i-protivopokazanija-kompleks-uprazhnenij-dlja-doma-video.html)"
+    ]
 
-    static let hardRecommendations = [String]()
+    static let hardRecommendations = [
+        "Экспериментируйте с пройденным расстоянием, возможно, Вы можете больше, чем думаете.",
+        "Работайте над техникой спуска и подъема на холмы, это крайне важно на Вашем уровне подготовки.",
+        "Чаще учавствуте в соревнованиях!!",
+        "Попробуйте методику [интервальных тренировок](https://my-fit.ru/fitness_guide/intervalnye-trenirovki---chto-eto-takoe-i-komu-podoidut/)"
+    ]
 }
